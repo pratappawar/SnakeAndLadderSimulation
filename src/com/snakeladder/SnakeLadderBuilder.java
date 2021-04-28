aa8f37e8c47678ea3f2adf1bdc8efbc7dc7169eb
Pratap Pawar <pratappawar1997@gmail.com>
2021-04-28 18:36:38 +0530

    Added: Checking for option like Ladder or Snake

--- src/com/snakeladder/SnakeLadderBuilder.java
+++ src/com/snakeladder/SnakeLadderBuilder.java
@@ -12,10 +12,38 @@ public class SnakeLadderBuilder {
 		int B_posTemp;
 		System.out.println("!!**Welcome to Snake and Ladder Simulation**!!");
 		
-		rollDice++;     //taking status for dice rolling
-		int Dice_A=(int)(Math.floor(Math.random()*10)%6 + 1);
-		System.out.println("Getting Dice number:"+Dice_A);
-
+			
+			rollDice++;     //taking status for dice rolling
+			int Dice_A=(int)(Math.floor(Math.random()*10)%6 + 1);
+			System.out.println("Getting Dice number:"+Dice_A);
+			
+			       //player A
+			int checkOpt=(int)(Math.random()*10)%3+1;
+			System.out.println("Checking choice:"+checkOpt);
+			
+			//checking ladder or snake checkOpt 1: ladder,checkOpt=2-->snake
+			
+			switch(checkOpt) {
+			case 1:{
+				System.out.println("Choice is for Ladder..");
+				A_posTemp=A_pos;
+				A_pos+=Dice_A;
+				System.out.println("Position of die now:"+A_pos);
+				break;
+			}
+			case 2:{
+				System.out.println("Choice is for Snake-->");
+				A_posTemp=A_pos;
+				A_pos-=Dice_A;
+				System.out.println("Position of die now:"+A_pos);
+				break;
+			}
+			default:
+				System.out.println("**NO PLAY**");
+			}
+		
+		
+	
 	}
 
 }
