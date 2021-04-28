cb1dc84dc3698863e75849c52923ac1872a47e72
Pratap Pawar <pratappawar1997@gmail.com>
2021-04-28 16:24:05 +0530

    Added: Welcome to Snake and Ladder Simulation

---
+++ src/com/snakeladder/SnakeLadderBuilder.java
@@ -0,0 +1,13 @@
+package com.snakeladder;
+
+public class SnakeLadderBuilder {
+
+	public static void main(String[] args) {
+		int A_pos=0;
+		int B_pos=0;
+		System.out.println("!!**Welcome to Snake and Ladder Simulation**!!");
+
+
+	}
+
+}
