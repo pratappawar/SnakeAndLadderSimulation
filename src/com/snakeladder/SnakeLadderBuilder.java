d0da0afff2bd2af2749dae94159757a4f69efe7e
Pratap Pawar <pratappawar1997@gmail.com>
2021-04-28 16:43:56 +0530

    Added: Snake Ladder Game Played by Single Player start pos 0

--- src/com/snakeladder/SnakeLadderBuilder.java
+++ src/com/snakeladder/SnakeLadderBuilder.java
@@ -5,7 +5,13 @@ public class SnakeLadderBuilder {
 	public static void main(String[] args) {
 		int A_pos=0;
 		int B_pos=0;
+		int A_flag=0;  //not play
+		int B_flag=1;  //play on
+		int rollDice=0;
+		int A_posTemp;
+		int B_posTemp;
 		System.out.println("!!**Welcome to Snake and Ladder Simulation**!!");
+		
 
 
 	}
