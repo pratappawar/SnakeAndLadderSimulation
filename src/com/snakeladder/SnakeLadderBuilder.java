1a401d1082275610b3f57d7e5550041342b46277
Pratap Pawar <pratappawar1997@gmail.com>
2021-04-29 18:24:47 +0530

    Added: Checking For option 1.Ladder 2.Snake 3.No Play

--- src/com/snakeladder/SnakeLadderBuilder.java
+++ src/com/snakeladder/SnakeLadderBuilder.java
@@ -14,8 +14,56 @@ public class SnakeLadderBuilder {
 		System.out.println("!!!Welcome to Snake Ladder Simulation!!!");
 		
 		int Die = (int)(Math.random()*6) + 1;
+		
 		No_Of_Die++;
-		System.out.println("Number get on Dice: "+Die);
+		System.out.println("Number got on Dice: "+Die);
+		
+		int CheckOpt = (int)(Math.random()*3) + 1;
+		//1:ladder 2:Snake 3:No play
+		System.out.println("Checking option: "+CheckOpt);
+		
+		switch(CheckOpt) {
+		case 1:           //Ladder
+			if(isPlayer) {
+				isPlayer=false;
+				playerA_pos=playerA_pos+Die;
+				System.out.println("Position of the player A: "+playerA_pos );
+			}
+			else {
+				isPlayer=true;
+				playerA_pos=playerA_pos+Die;
+				System.out.println("Position of the player B: "+playerB_pos );
+			}
+			break;
+			
+		case 2:           //Snake
+			if(isPlayer) {
+				isPlayer=false;
+				playerA_pos=playerA_pos-Die;
+				System.out.println("Position of the player A: "+playerA_pos );
+			}
+			else {
+				isPlayer=true;
+				playerA_pos=playerA_pos-Die;
+				System.out.println("Position of the player B : "+playerB_pos );
+			}
+			break;
+			
+		case 3:           //No play
+			if(isPlayer) {
+				isPlayer=false;
+				System.out.println("The player-->A Skip 1 Chance position is: "+playerA_pos );
+			}
+			else {
+				isPlayer=true;
+				System.out.println("The player-->B Skip 1 Chance position is: "+playerB_pos );
+			}
+			break;
+			
+			
+			
+		}
+		
 	}
 
 }
