37dfe377fc3f81a37c537385375fb3a65c22d4bc
Pratap Pawar <pratappawar1997@gmail.com>
2021-04-29 11:50:56 +0530

    Added: Player Reach Upto Final position

--- src/com/snakeladder/SnakeLadderBuilder.java
+++ src/com/snakeladder/SnakeLadderBuilder.java
@@ -1,3 +1,4 @@
+
 package com.snakeladder;
 
 public class SnakeLadderBuilder {
@@ -5,14 +6,11 @@ public class SnakeLadderBuilder {
 	public static void main(String[] args) {
 		int A_pos=0;
 		int B_pos=0;
-		int A_flag=0;  //not play
 		int B_flag=1;  //play on
 		int rollDice=0;
-		int A_posTemp;
-		int B_posTemp;
 		System.out.println("!!**Welcome to Snake and Ladder Simulation**!!");
 		
-			
+		while(A_pos <=100) {
 			rollDice++;     //taking status for dice rolling
 			int Dice_A=(int)(Math.floor(Math.random()*10)%6 + 1);
 			System.out.println("Getting Dice number:"+Dice_A);
@@ -43,7 +41,7 @@ public class SnakeLadderBuilder {
 			}
 		
 		
-	
+		}
 	}
 
 }
